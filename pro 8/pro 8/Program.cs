81e87aa88fbc24f0f639d2decc4da04ef057d7c1
jeweljose22 <117150116+jeweljose22@users.noreply.github.com>
2023-01-23 17:45:51 +0530

    Add files via upload

---
+++ pro 8/pro 8/Program.cs
@@ -0,0 +1,28 @@
+﻿Decimal[] fare = new decimal[3];
+string[] destination = new string[3];
+Console.WriteLine("enter fare");
+for(int i = 0;i < 3; i++)
+{
+    fare[i] =Convert.ToDecimal(Console.ReadLine());
+}
+Console.WriteLine("enter destination");
+for(int i = 0;i < 3; i++)
+{
+    destination[i]= Console.ReadLine();
+}
+Console.WriteLine("enter the pos");
+int pos= Convert.ToInt32(Console.ReadLine());
+if(pos < 4)
+{
+    Console.WriteLine(fare[pos-1]);
+    Console.WriteLine(destination[pos-1]);
+}
+else
+{
+    Console.WriteLine("not valid");   
+}
+
+    
+    //Console.WriteLine(destination[pos]);
+
+
